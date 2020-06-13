a7bf231bd69b47ad7e625a0c62ba48ee4e29220d
Lorenzo Laneve <lore97drk@icloud.com>
2020-06-13 13:14:35 +0200

    Remove transposition from visualize functions

--- util/visualize.py
+++ util/visualize.py
@@ -6,26 +6,26 @@ from PIL import Image
 
 # x - data matrix of shape (w, h, 3) corresponding to a single RGB image
 def view_image(x):
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(x, axes=[1, 0, 2]) * 255)))
+    plt.imshow(Image.fromarray(np.uint8(x * 255)))
 
 # y - data matrix of shape (w, h) corresponding to a single grayscale image
 def view_mask(y):
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(y) * 255), 'L'))
+    plt.imshow(Image.fromarray(np.uint8(y * 255), 'L'))
 
 # x - data matrix of shape (w, h, 3) corresponding to a single RGB image
 # y - data matrix of shape (w, h) corresponding to a single grayscale image
 def view_image_mask(x, y):
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(x, axes=[1, 0, 2]) * 255)))
+    plt.imshow(Image.fromarray(np.uint8(x * 255)))
     plt.figure()
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(y) * 255), 'L'))
+    plt.imshow(Image.fromarray(np.uint8(y * 255), 'L'))
 
 def view_image_mask2(x, y, y_true):
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(x, axes=[1, 0, 2]) * 255)))
+    plt.imshow(Image.fromarray(np.uint8(x * 255)))
     plt.figure()
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(y) * 255), 'L'))
+    plt.imshow(Image.fromarray(np.uint8(y * 255), 'L'))
     plt.figure()
-    plt.imshow(Image.fromarray(np.uint8(np.transpose(y_true) * 255), 'L'))
+    plt.imshow(Image.fromarray(np.uint8(y_true * 255), 'L'))
 
 
 def save_image(x, filename):
-    Image.fromarray(np.uint8(np.transpose(x, axes=[1, 0, 2]) * 255)).save(filename)
+    Image.fromarray(np.uint8(x * 255)).save(filename)
