10a8107ed9c67aceba940b8fe8b151392282f568
Lorenzo Laneve <lore97drk@icloud.com>
2020-06-14 17:26:38 +0200

    Increase batch size to 250

--- util/config.py
+++ util/config.py
@@ -6,7 +6,7 @@ from sklearn.metrics import f1_score
 # seed used for pseudo-random number generators
 RND_SEED = 1
 patch_size = 16
-batch_size = 125
+batch_size = 250
 steps_per_epoch = 200
 epochs = 20
 window_size = 72
