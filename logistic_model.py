858bef278872eed7cc3f9a94b3ceebb7100cbfaf
Lorenzo Laneve <lore97drk@icloud.com>
2020-07-18 18:24:53 +0200

    Update per-patch logistic classifier

--- logistic_model.py
+++ logistic_model.py
@@ -1,99 +1,77 @@
-# -*- coding: utf-8 -*-
 
 import numpy as np
-from sklearn import linear_model
+
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import PolynomialFeatures
-from util.helpers import *
+
 from util.model_base import ModelBase
 
+def patchify(X):
+    patches = []
+
+    width = X.shape[0]
+    height = X.shape[1]
+
+    for y in range(0, height, 16):
+        for x in range(0, width, 16):
+            patches.append(X[x:x+16, y:y+16, :])
+
+    return patches
+
+def patchify_gt(X):
+    patches = []
+
+    width = X.shape[0]
+    height = X.shape[1]
+
+    for y in range(0, height, 16):
+        for x in range(0, width, 16):
+            patches.append((np.mean(X[x:x+16, y:y+16]) >= 0.25) * 1)
+
+    return patches
+
 class LogisticModel(ModelBase):
-    
+
     def __init__(self):
-        """
-        Construct a logistic regression classifier.
-        """
-        self.patch_size = 16
+        self.model = None
 
     def initialize(self):
-        """ Initialize or reset this model. """
-        self.logreg = linear_model.LogisticRegression(C=1e5, max_iter = 500)
-    
-    def extract_features(self, img):
-        """
-        Extract features from a patch:
-        - Average color for each RGB channel
-        - Standard deviation for each RGB channel
-        """
-        feat_m = np.mean(img, axis=(0,1))
-        feat_v = np.std(img, axis=(0,1))
-        feat = np.append(feat_m, feat_v)
-        return feat
-    
-    def poly_fit(self, X):
-        """
-        Fit the dataset using a polynomial basis.
-        """
-        poly = PolynomialFeatures(4, interaction_only=False)
-        return poly.fit_transform(X)
-    
-    # Extract features for a given image
-    def extract_img_features(self, filename):
-        img = load_image(filename)
-        img_patches = img_crop(img, self.patch_size, self.patch_size)
-        X = np.asarray([self.extract_features(img_patches[i]) for i in range(len(img_patches))])
-        return X
-    
+        self.model = LogisticRegression(C=1e5, max_iter = 500)
+
+    def patches_to_features(self, X_patches):
+        # Basic features as suggested in project statement
+        X_mean = np.mean(X_patches, axis=(1,2))
+        X_std = np.std(X_patches, axis=(1,2))
+        X = np.append(X_mean, X_std)
+
+        # Polynomial expansion of features
+        poly_expansion = PolynomialFeatures(5, interaction_only=False)
+        return poly_expansion.fit_transform(X)
+
+    def decompose(self, Y, X):
+        X_patches = []
+        Y_patches = []
+
+        for i in range(X.shape[0]):
+            X_patches += patchify(X[i])
+            Y_patches += patchify_gt(Y[i])
+
+        X_patches = np.array(X_patches)
+        Y_patches = np.array(Y_patches)
+
+        return Y_patches, self.patches_to_features(X_patches)
+
     def train(self, Y, X):
-        """
-        Train this model.
-        """
-        
-        foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+        Y_f, X_f = self.decompose(Y, X)
 
-        def value_to_class(v):
-            df = np.sum(v)
-            if df > foreground_threshold:
-                return 1
-            else:
-                return 0
-        
-        # Extract patches from input images
-        patch_size = self.patch_size
-        img_patches = [img_crop(X[i], patch_size, patch_size, patch_size, 0) for i in range(X.shape[0])]
-        gt_patches = [img_crop_gt(Y[i], patch_size, patch_size, patch_size) for i in range(X.shape[0])]
+        self.model.fit(Y_f, X_f)
 
-        # Linearize list of patches
-        img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
-        gt_patches =  np.asarray([gt_patches[i][j] for i in range(len(gt_patches)) for j in range(len(gt_patches[i]))])
-        
-        X = np.asarray([self.extract_features(img_patches[i]) for i in range(len(img_patches))])
-        Y = np.asarray([value_to_class(np.mean(gt_patches[i])) for i in range(len(gt_patches))])
-           
-        X = self.poly_fit(X)
-        self.logreg.fit(X, Y)
-        
-    def save(self, filename):
-        # Nothing to do
-        return
-        
-    def load(self, filename):
-        # Nothing to do
-        return
-        
     def classify(self, X):
-        """
-        Classify an unseen set of samples.
-        This method must be called after "train".
-        Returns a list of predictions.
-        """
-        num_of_img = X.shape[0]
-        img_size = (X.shape[1], X.shape[2])
+        X_patches = []
 
-        patch_size = self.patch_size
-        img_patches = [img_crop(X[i], patch_size, patch_size, patch_size, 0) for i in range(X.shape[0])]
-        img_patches = np.asarray([img_patches[i][j] for i in range(len(img_patches)) for j in range(len(img_patches[i]))])
-        X = np.asarray([self.extract_features(img_patches[i]) for i in range(len(img_patches))])
-        X = self.poly_fit(X)
-        Z = self.logreg.predict(X)
+        for i in range(X.shape[0]):
+            X_patches += patchify(X[i])
 
-        return recompose(Z, num_of_img, img_size, patch_size)
+        X_features = self.patches_to_features(np.array(X_patches))
+
+        return self.model.predict(X_features)
