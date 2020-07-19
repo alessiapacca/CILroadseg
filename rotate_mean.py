1dc5782cc5ac56fa1e7010d2e4c09c3ccaddb99c
Lorenzo Laneve <lore97drk@icloud.com>
2020-07-19 15:17:55 +0200

    Add new rotate and mean decorator

--- rotate_mean.py
+++ rotate_mean.py
@@ -4,18 +4,16 @@ import numpy as np
 from util.model_base import ModelBase
 
 #
-# Rotate and Mean. For each classification, the classification is done for the eight
+# Rotate and Mean decorator. For each classification, the classification is done for the eight
 # possible rotations/flip combinations and then the mean of predictions is returned.
 #
 class RotAndMean(ModelBase):
 
     #
     # model - model used for classification
-    # include_flips - whether also flips should be included
     #
-    def __init__(self, model, include_flips=True):
+    def __init__(self, model):
         self.model = model
-        self.include_flips = include_flips
 
     def initialize(self):
         self.model.initialize()
@@ -29,25 +27,21 @@ class RotAndMean(ModelBase):
     def classify(self, X):
         model = self.model
         num_samples = X.shape[0]
-        preds_per_sample = 8 if self.include_flips else 4
 
-        X_rots = np.empty((preds_per_sample*num_samples, X.shape[1], X.shape[2], X.shape[3]))
+        X_rots = np.empty((8*num_samples, X.shape[1], X.shape[2], X.shape[3]))
 
         for i in range(4):
-            X_rots[i*num_samples:(i+1)*num_samples] = np.rot90(X, i, axes=(1, 2))
+            X_rots[i*num_samples:(i+1)*num_samples] = np.rot90(X, i+1, axes=(1, 2))
 
-        if self.include_flips: # copy flipped images on the second part of array
-            for i in range(4*num_samples):
-                X_rots[4*num_samples + i] = np.fliplr(X_rots[i])
+        for i in range(4):
+            X_rots[(i+4)*num_samples:(i+5)*num_samples] = np.fliplr(np.rot90(X, i+1, axes=(1, 2)))
 
         Y_pred = model.classify(X_rots)
+        Y_pred = Y_pred.reshape((48, num_samples))
 
-        if Y_pred.ndim == 1: # patchwise predictions
-            Y_pred = Y_pred.reshape((preds_per_sample, num_samples))
-        else: # imagewise prediction
-            Y_pred = Y_pred.reshape((preds_per_sample, num_samples, X.shape[1], X.shape[2], X.shape[3]))
-
-        return np.mean(Y_pred, axis=0)
+        Y_pred = np.mean(Y_pred, axis=0)
+        Y_pred = (Y_pred >= 0.5) * 1
+        return Y_pred
 
     def save(self, filename):
         self.model.save(filename)
