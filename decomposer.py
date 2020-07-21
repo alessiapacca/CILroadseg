72f0e84e1f2bec56bad259909bcca213d4995fe6
Lorenzo Laneve <lore97drk@icloud.com>
2020-07-21 13:38:11 +0200

    Update xception

--- decomposer.py
+++ decomposer.py
@@ -21,13 +21,9 @@ class Decomposer(ModelBase):
     # model - model that takes a window (window_size * window_size) and returns
     #         a patch (patch_size * patch_size)
     # window_size - size of the image window the inner model will take into account in order to do a classification
-    # focus_size - size of the focus of a window (i.e. the part in the center of the window that will be classified
-    #              by looking at the whole window)
     #
-    def __init__(self, model, augment_brightness=True):
+    def __init__(self, model, window_size):
         self.model = model # (window) -> (patch)
-
-        self.augment_brightness = augment_brightness
 
         self.focus_size = focus_size
         self.window_size = window_size
@@ -62,11 +58,6 @@ class Decomposer(ModelBase):
         flip = np.random.choice(2)
         rot_step = np.random.choice(4)
 
-        # data augmentation: random brightness factor (-10%, +20%)
-        if self.augment_brightness:
-            brightness_factor = 1 + (np.random.randint(-100, 200) / 100)
-            X_sample = np.clip(X_sample * brightness_factor, 0, 1)
-
         if flip: X_sample = np.fliplr(X_sample)
         X_sample = np.rot90(X_sample, rot_step)
         return Y_sample, X_sample
@@ -74,6 +65,8 @@ class Decomposer(ModelBase):
     def train(self, Y, X):
         padding = self.padding
 
+        # pad the image with reflect boundary conditions
+        # in order to be able to sample the corners of the image
         X_pad = np.empty((X.shape[0], X.shape[1] + 2*padding, X.shape[2] + 2*padding, X.shape[3]))
         Y_pad = np.empty((Y.shape[0], Y.shape[1] + 2*padding, Y.shape[2] + 2*padding))
 
@@ -86,13 +79,7 @@ class Decomposer(ModelBase):
                 img_id = np.random.choice(X.shape[0])
                 yield self.sample_window(Y[img_id], X[img_id])
 
-        perm = np.random.permutation(X.shape[0])
-
-        val_split = int(0.9 * X.shape[0])
-        Itr = perm[0: val_split]
-        Ival = perm[val_split: X.shape[0]]
-
-        self.train_online(bootstrap(Y_pad[Itr], X_pad[Itr]), bootstrap(Y_pad[Ival], X_pad[Ival]))
+        self.train_online(bootstrap(Y_pad, X_pad), bootstrap(Y_pad, X_pad))
 
     def train_online(self, generator, val_generator = None):
         self.model.train_online(generator, val_generator)
--- xception.py
+++ xception.py
@@ -8,15 +8,9 @@ from decomposer import *
 from util.visualize import *
 
 
-window_size = 200
-
-batch_size = 50
-val_batch_size = 1000
-
-steps_per_epoch = 200
-epochs = 20
-
 def batch_generator(bootstrap):
+    batch_size = 50
+
     while 1:
         # create one batch
         X_batch = np.empty((batch_size, window_size, window_size, 3))
@@ -29,6 +23,8 @@ def batch_generator(bootstrap):
 
 
 def val_batch_generator(bootstrap):
+    val_batch_size = 1000
+
     while 1:
         # create one batch
         X_batch = np.empty((val_batch_size, window_size, window_size, 3))
@@ -43,13 +39,20 @@ class XceptionModel(ModelBase):
 
     def __init__(self):
         self.model = None
+        self.window_size = 200
+
+        self.epochs = 20
+        self.steps_per_epoch = 200
 
     def initialize(self):
-        input_shape = (window_size, window_size, 3)
+        input_shape = (self.window_size, self.window_size, 3)
 
-        xcept = Xception(weights="imagenet", include_top=False, input_shape=input_shape, classes=1)
+        xcept = Xception(weights='imagenet', include_top=False, input_shape=input_shape)
 
-        for layer in xcept.layers[0:]:
+        for layer in xcept.layers[:20]:
+            layer.trainable = False
+
+        for layer in xcept.layers[20:]:
             layer.trainable = True
 
         layers = [
@@ -57,9 +60,9 @@ class XceptionModel(ModelBase):
 
             Flatten(),
             Dense(64, activation='relu', input_dim=input_shape),
-            Dropout(0.3),
+            Dropout(0.15),
             Dense(64, activation='relu'),
-            Dropout(0.2),
+            Dropout(0.15),
             Dense(1, activation='sigmoid')
         ]
 
@@ -75,20 +78,20 @@ class XceptionModel(ModelBase):
         # this generator does the bootstrap of a single sample.
         # batch_generator will create batches of these samples
 
+        self.model.summary()
         self.model.compile(optimizer=Adam(lr=0.001), loss='binary_crossentropy', metrics=['accuracy'])
 
         callbacks = [
-            ReduceLROnPlateau(monitor='val_accuracy', min_delta=0.0001, patience=4,
-                              verbose=1, mode='auto', factor=0.5, cooldown=0, min_lr=0),
-            EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=10,
-                          verbose=1, mode='auto')
+            ReduceLROnPlateau(monitor='val_accuracy', min_delta=0.0001, patience=4, verbose=1, factor=0.5),
+            EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=10, verbose=1)
         ]
 
-        X_val, Y_val = next(val_batch_generator(generator))
+        X_val, Y_val = next(val_batch_generator(val_generator))
 
+        np.random.seed(3)
         history = self.model.fit(batch_generator(generator),
-                                 steps_per_epoch=steps_per_epoch,
-                                 epochs=epochs,
+                                 steps_per_epoch=self.steps_per_epoch,
+                                 epochs=self.epochs,
                                  verbose=1,
                                  callbacks=callbacks,
                                  validation_data=(X_val, Y_val))
